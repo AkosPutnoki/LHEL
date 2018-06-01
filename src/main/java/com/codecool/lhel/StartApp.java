3f39addf0ef99a8be9c03634d038e52fd1e8cc69
Akos Putnoki <putnokiakos@gmail.com>
2018-06-01 14:22:01 +0200

    git init

---
+++ src/main/java/com/codecool/lhel/StartApp.java
@@ -0,0 +1,15 @@
+package com.codecool.lhel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StartApp.class, args);
+
+    }
+
+}
